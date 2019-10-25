2a3304c46ef7bb35f6ae190a467d9528d91d6ae8
Boris Filippov <krenzyfryger@gmail.com>
2019-10-26 02:24:24 +0400

    Test community baseline on 10 fold CV

--- dsb2019/dsb2019/data/validation.py
+++ dsb2019/dsb2019/data/validation.py
@@ -1,3 +1,17 @@
-from sklearn.croos
+import numpy as np
+from sklearn.model_selection import GroupKFold, KFold
+from sklearn.utils import shuffle
 
-class InstallationFold(KFold)
+
+class InstallationFold(GroupKFold):
+    def __init__(self, installation_ids=None):
+        super().__init__(n_splits=10)
+        self.installation_ids = installation_ids
+
+    def split(self, X, y, installation_ids=None):
+        if installation_ids is None:
+            installation_ids = self.installation_ids
+        orig_indices = np.arange(len(X))
+        shuffled_indices, installation_ids = shuffle(orig_indices, installation_ids, random_state=2019)
+        for train, test in super().split(shuffled_indices, shuffled_indices, installation_ids):
+            yield shuffled_indices[train], shuffled_indices[test]
