26a90a9dbf31a904b6db5a3e8573c9209c88f4c2
Boris Filippov <krenzyfryger@gmail.com>
2019-10-27 02:19:18 +0400

    WIP game baseline

--- dsb2019/dsb2019/data/validation.py
+++ dsb2019/dsb2019/data/validation.py
@@ -1,6 +1,14 @@
 import numpy as np
 from sklearn.model_selection import GroupKFold, KFold
 from sklearn.utils import shuffle
+from typing import NamedTuple
+from functools import partial
+from sklearn.metrics import cohen_kappa_score
+
+
+class Predict(NamedTuple):
+    true: np.array
+    pred: np.array
 
 
 class InstallationFold(GroupKFold):
@@ -15,3 +23,23 @@ class InstallationFold(GroupKFold):
         shuffled_indices, installation_ids = shuffle(orig_indices, installation_ids, random_state=2019)
         for train, test in super().split(shuffled_indices, shuffled_indices, installation_ids):
             yield shuffled_indices[train], shuffled_indices[test]
+
+
+def fit_fold(df, train_ix, test_ix, make_features, train_model, make_predictions):
+    train = df.iloc[train_ix].reset_index().copy()
+    test = df.iloc[test_ix].reset_index().copy()
+    train_features, test_features = make_features(train, test)
+    model = train_model(*train_features)
+    test_pred, test_true = make_predictions(model, *test_features)
+    return Predict(test_true, test_pred)
+
+
+def cross_validate(train, labels, make_features, train_model, make_predictions):
+    predicts = []
+    cv = InstallationFold()
+    for ix_train, ix_test in cv.split(train, labels, train.installation_id.values):
+        predicts.append(fit_fold(train, ix_train, ix_test, make_features, train_model, make_predictions))
+    return predicts
+
+
+quad_kappa = partial(cohen_kappa_score, weights="quadratic")
