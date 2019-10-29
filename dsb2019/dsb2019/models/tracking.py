9718c81cd4455e9d48f8e74c568bd144a01634f1
Boris Filippov <krenzyfryger@gmail.com>
2019-10-30 01:44:10 +0400

    Add time info

--- dsb2019/dsb2019/models/tracking.py
+++ dsb2019/dsb2019/models/tracking.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import numpy as np
 import os
+from datetime import datetime
 from pathlib import Path
+from pytz import timezone
+
 
 TRACKING_FILE = Path(os.getenv("TRACKING_FILE", "/code/dsb2019/tracking.csv"))
+TZ = timezone("Europe/Samara")
 
 
 def track_experiment(name: str, mean_score: float, cv_scores: np.array, notebook_path: str) -> pd.DataFrame:
-    experiment = pd.DataFrame(data={"name": [name], "mean_score": [mean_score], "cv_scores": [cv_scores], 
+    experiment = pd.DataFrame(data={"name": [name], "time": [datetime.now(TZ)], "mean_score": [mean_score], "cv_scores": [cv_scores], 
                                     "notebook_path": [notebook_path], "submission_path": [None], "submission_score": [None]})
     if TRACKING_FILE.exists():
         tracking_df = pd.read_csv(TRACKING_FILE)
+        if list(tracking_df.columns) != list(experiment.columns):
+            raise RuntimeError("Tracking schema changed!")
         experiment_mask = tracking_df.name.isin([name]) 
         if experiment_mask.sum() > 0:
             tracking_df[experiment_mask] = experiment
